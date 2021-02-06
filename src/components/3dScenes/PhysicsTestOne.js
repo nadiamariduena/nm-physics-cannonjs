29c7387b78185a0c5ccdc0c3e9325a87222c8c39
melissa <nadia.mm.mariduena@gmail.com>
2021-02-06 19:59:58 +0100

    installing cannon js | cleaning the scene

---
+++ src/components/3dScenes/PhysicsTestOne.js
@@ -0,0 +1,234 @@
+import React, { Component } from "react";
+import * as THREE from "three";
+// import { DragControls } from "three/examples/jsm/controls/DragControls";
+//
+//
+const style = {
+  height: 600, // we can control scene size by setting container dimensions
+};
+//
+
+class physicsTestOne extends Component {
+  componentDidMount() {
+    // Here you are calling all the functions below
+    this.sceneSetup();
+    this.addCustomSceneObjects();
+    this.startAnimationLoop();
+    //
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+  //
+  //
+  componentWillUnmount() {
+    // this is related to the event listeners that cause problems when using the resizing
+    window.removeEventListener("resize", this.handleWindowResize);
+    window.cancelAnimationFrame(this.requestID);
+
+    // this.controls.dispose();
+  }
+  /*
+
+
+                                            ***  1   ***
+
+
+
+  */
+  sceneSetup = () => {
+    //
+    //                WIDTH/HEIGHT
+    // --------------------------------------------
+    //
+    const width = this.eleModelBlOne.clientWidth;
+    const height = this.eleModelBlOne.clientHeight;
+    //
+    // --------------------------------------------
+
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    this.camera.position.y = 10;
+    //
+    // this.camera.position.z = 3;
+    //
+    this.scene = new THREE.Scene();
+    // this.scene.background = new THREE.Color(0xffffff);
+    //
+    //
+    this.loader = new THREE.TextureLoader();
+    //
+    // ---------------------------------------
+    //                  RENDERER
+    // ---------------------------------------
+    //
+    this.renderer = new THREE.WebGL1Renderer({
+      alpha: true,
+      // will make the edges smooth
+      antialias: true,
+    });
+    //
+    this.renderer.setSize(width, height);
+    // BG color from the scene
+    // showMap is connected to the shadows in any object/model
+    this.renderer.shadowMap.enabled = true;
+    // here you append it to the jsx
+    this.eleModelBlOne.appendChild(this.renderer.domElement); // mount using React ref
+    // document.appendChild(this.renderer.domElement);  //before
+    //
+
+    //
+    //
+
+    //
+    //
+  };
+
+  /*
+
+
+                                            ***  2  ***
+
+
+
+  */
+  addCustomSceneObjects = () => {
+    //
+
+    //
+    //
+
+    //
+    //
+
+    //
+    //---------------------------
+    //    DRAG CONTROLS
+    //---------------------------
+
+    //
+    //
+
+    //----------------------------------
+    //         BLENDER  MODELS
+    //----------------------------------
+    //
+
+    //---------------------
+    //   Directional Light
+    //---------------------
+    //
+    // //
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.autoUpdate = true;
+    this.renderer.gammaFactor = 2.2;
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(5, -1, 100);
+
+    // position as follow , the light comes from x:-1000, comes from: y and the last comes from : z
+    directionalLight.position.set(1000, 1000, 1000);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera = new THREE.OrthographicCamera(
+      -100,
+      200,
+      -200,
+      200,
+      0.5,
+      5000
+    );
+    // //
+    this.scene.add(directionalLight);
+    // The light points to the flat ground
+    // this.directionalLight.target = this.plane;  //dont add this
+    //
+    //
+    //THIS LIGHT IS ON THE BOTTOM
+    //---------------------
+    //     spotLight FF5733
+    //---------------------
+    //
+
+    // With the light you can see the colors you added to each geometry in the materials
+    this.spotLight = new THREE.SpotLight(0xffffff, 0.5); //intensity:   0.5);
+    // spotLight.position.set( 0 , 10 , 0 );
+    this.spotLight.position.set(5, -50, 0); //x, y , z   original (5, -50, 0);
+    // (2, 32, 32); with this settings the light will be on the front
+    this.spotLight.castShadow = true;
+    //
+    // this will remove the shadows
+    this.spotLight.visible = true;
+    //
+    this.scene.add(this.spotLight);
+    // //
+    //
+    //
+  };
+  /*
+
+
+                                            ***  3   ***
+
+
+
+  */
+
+  startAnimationLoop = () => {
+    //
+
+    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
+
+    this.renderer.render(this.scene, this.camera);
+  };
+  /*
+
+
+
+
+
+
+  */
+  handleWindowResize = () => {
+    const width = this.eleModelBlOne.clientWidth;
+    const height = this.eleModelBlOne.clientHeight;
+    //
+    // updated renderer
+    this.renderer.setSize(width, height);
+    // updated **camera** aspect ratio
+    this.camera.aspect = width / height;
+    // That is the Three.js optimization: you can group multiple camera changes into a block with only one
+    this.camera.updateProjectionMatrix();
+  };
+  /*
+
+
+
+
+
+
+  */
+
+  render() {
+    return (
+      <div className="scene-oblivion">
+        {/* --------------------- */}
+
+        {/* --------------------- */}
+        {/* --------------------- */}
+        <div
+          className="modelBleOne"
+          style={style}
+          ref={(ref) => (this.eleModelBlOne = ref)}
+        ></div>
+        {/* --------------------- */}
+        {/* --------------------- */}
+        {/* --------------------- */}
+      </div>
+    );
+  }
+}
+
+export default physicsTestOne;
---
+++ src/components/home/Home.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+import Header from "../navigation/Header";
+import PhysicsTestOne from "../3dScenes/PhysicsTestOne";
+// import HomePortfolioGallery from "./HomePortfolioGallery";
+// import ContactSection from "./HomeContact";
+// import FooterTextAnimation from "./HomeFooterText";
+
+//
+
+function Home() {
+  return (
+    <React.Fragment>
+      {/* --------------- */}
+      {/* --------------- */}
+      {/* --------------- */}
+      {/* <Header /> */}
+      {/* --------------- */}
+      {/* --------------- */}
+      {/* --------------- */}
+      <section className="container-section-scene-home">
+        {/* ----------------------------------------- */}
+        {/*             FLAG SECTION                  */}
+        {/* ----------------------------------------- */}
+        <div className="scene-threejs">
+          <div className="wrapper-flag-scene-threejs">
+            {/* ----------------------------------------- */}
+
+            <div className="wrapper-scene-oblivion">
+              <PhysicsTestOne />
+            </div>
+            {/* ----------------------------------------- */}
+
+            {/* <div className="scene-description-home">
+              <div className="wrapper-scene-description-home">
+                <h3 className="h3-text-img-home">3D TESTS</h3>
+                <p>
+                  I created this 3d scenes using threejs during my time at DCI.
+                  Additional information about the references used for this
+                  project, can be found in my github.
+                </p>
+              </div>
+            </div> */}
+            {/* ----------------------------------------- */}
+          </div>
+        </div>
+      </section>
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* <section className="container-gallery">
+        <HomePortfolioGallery />
+      </section> */}
+
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+      {/* ----------------------------------------- */}
+
+      {/* ----------------------------------------- */}
+      {/*             CONTACT SECTION               */}
+      {/* ----------------------------------------- */}
+      {/* <ContactSection /> */}
+      {/* --------------- */}
+      {/* <FooterTextAnimation /> */}
+    </React.Fragment>
+  );
+}
+
+export default Home;
